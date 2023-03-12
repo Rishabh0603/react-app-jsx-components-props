c5940a20a43dc7d1063c0aff0f8414114d165eb3
Rishabh0603 <rishabhtrivedilko.50@gmail.com>
2023-03-12 14:10:33 +0530

    More functionality added
    Now no. of words and no. of characters can be seen

--- starting_with_react/src/component/TextArea.js
+++ starting_with_react/src/component/TextArea.js
@@ -2,21 +2,39 @@ import React,{useState} from 'react';
 
 export default function TextArea(props){
     const[text,setText] = useState("");
-
-    const SetonClick= ()=>{
+    // const[length,setLength] =useState(0);
+ 
+    const SettoUppercase= ()=>{
         let changeCase = text.toUpperCase();
         
         setText(changeCase);
     }
+    const SettoLowerCase=()=>{
+       let changeCase = text.toLowerCase();
+       setText(changeCase);
+    }
     const setOnChange= (text) =>{
         setText(text.target.value);
     }
+    // const wordCount = ()=>{
+    //    let word_count = 0;
+    //    if(text==="")
+    //    setLength(0);
+    //    else
+    //     word_count = text.split(" ").length;
+    //    setLength(word_count);
+        
+      
+    // }
     return(
         <div>
             <h1>{props.heading}</h1>
       <div><label htmlFor="text1">{props.text}</label></div> 
       <textarea placeholder="enter text here" type="text" id="text1" rows="10" onChange={setOnChange} value={text} /><br/>
-      <button onClick={SetonClick} >Convert to UpperCase</button>
+      <button onClick={SettoUppercase} >Convert to UpperCase</button>
+      <button onClick={SettoLowerCase}>Convert to LowerCase</button>
+      <h3>Count Words and Characters</h3>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
       </div>
     )
 }
